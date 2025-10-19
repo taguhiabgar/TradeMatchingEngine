6fbd18110e1cb330bb2d0edb493a569063d65589
Taguhi Abgar <taguhiabgar@gmail.com>
2025-10-19 15:50:36 +0400

    Implemented processOrder in Matching Engine, added functions for adding resting order, matching buy and sell orders (no trade execution yet).

--- src/engine/MatchingEngine.cpp
+++ src/engine/MatchingEngine.cpp
@@ -1,11 +1,74 @@
 #include "MatchingEngine.h"
-#include "Order.h"
-
 
 namespace engine {
 
 void MatchingEngine::processOrder(const Order& order) {
+    std::cout << "process order: " << order << std::endl;
+    if (order.isBuy()) {
+        matchBuyOrder(order);
+    } else {
+        matchSellOrder(order);
+    }
+}
+
+// TODO: replace int with Trade object
+std::vector<int> MatchingEngine::matchBuyOrder(const Order& aggressor) noexcept {
+    // TODO: replace int with Trade object
+    // std::vector<int> trades = matchOrderInternal(aggressor, sells);
+    return matchOrderInternal(aggressor, sells);
     
+}
+
+// TODO: replace int with Trade object
+std::vector<int> MatchingEngine::matchSellOrder(const Order& aggressor) noexcept {
+    // TODO: replace int with Trade object
+    //std::vector<int> trades = matchOrderInternal(aggressor, buys);
+    return matchOrderInternal(aggressor, buys);
+    
+}
+
+// TODO: replace int with Trade object
+template <typename Map>
+std::vector<int> MatchingEngine::matchOrderInternal(const Order& aggressor, Map& restingOrders)
+{
+    if (restingOrders.empty()) {
+        addRestingOrder(aggressor);
+        return {};
+    }
+    
+    auto [bestPrice, bestOrder] = *restingOrders.begin();
+    
+    auto compareFunc = restingOrders.key_comp();
+    if (compareFunc(aggressor.price, bestPrice)) { // match was made
+        std::cout << "trade" << std::endl;
+        return {0};
+    } else { // no match
+        std::cout << "no trade - make resting order" << std::endl;
+        restingOrders[aggressor.price] = aggressor;
+    }
+    
+    std::cout << "bestPrice: " << bestPrice << ", bestOrder: " << bestOrder << std::endl;
+    
+    for (const auto& [price, order] : restingOrders) {
+        std::cout << order << '\n';
+    }
+    
+    if (aggressor.quantity > 0) {
+        addRestingOrder(aggressor);
+        
+    }
+    
+    return {};
+}
+
+void MatchingEngine::addRestingOrder(const Order& order) noexcept {
+    std::cout << "add resting order: " << order << std::endl;
+    
+    if (order.isBuy()) {
+        buys.emplace(order.price, order);
+    } else {
+        sells.emplace(order.price, order);
+    }
 }
 
 
--- src/engine/MatchingEngine.h
+++ src/engine/MatchingEngine.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Order.h"
+
+#include <map>
+#include <vector>
+
 namespace engine {
 
 // Central class coordinating buy/sell order processing
@@ -7,6 +12,19 @@ namespace engine {
 class MatchingEngine {
 public:
     void processOrder(const Order& order);
+
+private:
+    std::vector<int> matchBuyOrder(const Order& aggressor) noexcept;
+    std::vector<int> matchSellOrder(const Order& aggressor) noexcept;
+    void addRestingOrder(const Order& aggressor) noexcept;
+    
+    // TODO: replace int with Trade object
+    template <typename Map>
+    std::vector<int> matchOrderInternal(const Order& aggressor, Map& restingOrders);
+    
+private:
+    std::map<unsigned, Order, std::greater<unsigned>> buys;
+    std::map<unsigned, Order, std::less<unsigned>> sells;
 };
 
 } // namespace engine
